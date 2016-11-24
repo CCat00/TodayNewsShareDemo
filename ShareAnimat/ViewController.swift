7f8de0cb036f0c98b82cba0bfdea914a47348300
hanwei <1019260351@qq.com>
2016-11-24 10:19:49 +0800

    init project

---
+++ ShareAnimat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShareAnimat
+//
+//  Created by 韩威 on 2016/11/24.
+//  Copyright © 2016年 Wei Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
